10edef9be6f09000c0ef403fd7ea5f31257e7165
Antoine <ahoorelbeke@gmail.com>
2019-01-22 17:37:13 +0100

    Add otheralgorithms

--- agent.py
+++ agent.py
@@ -1,8 +1,10 @@
 import numpy as np
 from gym.spaces.discrete import Discrete
 
+N_STEP_ALGORITHMS = ['tb', 'is', 'retrace', 'q_lambda']
+
 class Agent():
-    def __init__(self, env=None, state_space=['start', 'end'], state_terminal = [False, True], action_space=['a', 'b'], alpha=0.01, epsilon=0.5, gamma=0.8, n=100, algorithm_used='tb', verbose=False):
+    def __init__(self, env=None, state_space=['start', 'end'], state_terminal = [False, True], action_space=['a', 'b'], alpha=0.01, epsilon=0.5, gamma=0.8, lbda=1, n=100, evaluate_every=5000, episodes_to_evaluate=200, algorithm_used='tb', verbose=False):
         if env is not None:
             self.init_with_env(env)
         else:
@@ -13,8 +15,12 @@ class Agent():
         self.epsilon = epsilon
         assert gamma > 0 and gamma <= 1
         self.gamma = gamma
+        assert lbda > 0
+        self.lbda = lbda
         self.n = n
         self.algorithm_used = algorithm_used
+        self.evaluate_every = evaluate_every
+        self.episodes_to_evaluate = episodes_to_evaluate
         self.q = 10e-3 * np.random.rand(self.state_count, self.action_count)
         self.verbose = verbose
         self.operations = {
@@ -24,6 +30,8 @@ class Agent():
             'delta': np.nan * np.ones(shape=(n,)),
             'c': np.nan * np.ones(shape=(n,)),
         }
+        self.rewards = []
+        self.avg_rewards = []
 
     def init_with_env(self, env):
         if type(env.action_space) != Discrete or type(env.observation_space) != Discrete:
@@ -67,21 +75,48 @@ class Agent():
         return np.argmax(self.q[state_index, :])
 
     def run_multiple_episode(self, number=100):
-        self.n_episode = 0
-        while self.n_episode <= number:
-            self.n_episode += 1
-            self.epsilon = np.maximum(0.01, self.epsilon / (self.n_episode)**(0.1))
+        print('')
+        print('******')
+        print('Training with algorithm: {}'.format(self.algorithm_used))
+        n_episode = 0
+        while n_episode <= number:
+            n_episode += 1
+            # decrease epsilon
+            self.epsilon = np.minimum(self.epsilon / (n_episode)**(0.01), 10e-2)
             self.run_episode(state_index=np.random.randint(0, self.state_count))
+            self.rewards.append(self.episode_reward) 
+            if n_episode % self.evaluate_every == 0:
+                avg_reward = 0
+                for _ in range(self.episodes_to_evaluate):
+                    avg_reward += self.play_episode()
+                avg_reward *= 1.0 / self.episodes_to_evaluate
+                print('Evaluating after {} episodes. Avg reward: {}'.format(n_episode, avg_reward))
+                self.avg_rewards.append(avg_reward)
+        print('******')
+        print('')
 
     def run_episode(self, state_index=None):
-        self.init_episode(state_index=state_index)
-        while self.tho < self.T - 1:
-            if self.t < self.T:
-                self.act_and_store()
-            self.tho = self.t - self.n + 1
-            if self.tho >= 0:
-                self.update_q()
-            self.t += 1
+        if self.algorithm_used in N_STEP_ALGORITHMS:
+            self.init_episode(state_index=state_index)
+            while self.tho < self.T - 1:
+                if self.t < self.T:
+                    self.act_and_store()
+                self.tho = self.t - self.n + 1
+                if self.tho >= 0:
+                    self.update_q()
+                self.t += 1
+        elif self.algorithm_used == 'q_learning':
+            is_terminal = False
+            self.episode_reward = 0
+            state_index = self.init_state_index(state_index=state_index)
+            while not is_terminal:
+                action_index = self.get_action_index_with_q_policy(state_index, eps_greedy=True)
+                reward, next_state_index, is_terminal = self.take_action(action_index)
+                self.q[state_index, action_index] += self.alpha * (reward + self.gamma * np.max(self.q[next_state_index, :]) - self.q[state_index, action_index])
+                state_index = next_state_index
+        else:
+            raise Exception("Algortihm {} isn't a valid one".format(self.algorithm_used))
+
 
     def init_episode(self, state_index=None):
         self.operations = {
@@ -93,12 +128,13 @@ class Agent():
         }
         state_index = self.init_state_index(state_index=state_index)
         self.set_operation('state', 0, state_index)
-        action_index = self.get_action_index_with_q_policy(state_index,eps_greedy=True)
+        action_index = self.get_action_index_with_q_policy(state_index, eps_greedy=True)
         self.set_operation('action', 0, action_index)
         self.set_operation('q', 0, self.q[state_index, action_index])
         self.T = np.inf
         self.tho = -1
         self.t = 0
+        self.episode_reward = 0
 
     def init_state_index(self, state_index=None):
         if self.is_init_with_env:
@@ -118,12 +154,13 @@ class Agent():
 
     def take_action(self, action_index):
         if self.is_init_with_env:
-            next_state_index, reward, is_terminal, _ = self.env.step(self.action_space[action_index])
-            # next_state_index = np.argwhere(self.state_space==next_state)[0]
+            next_state, reward, is_terminal, _ = self.env.step(self.action_space[action_index])
+            next_state_index = np.argwhere(self.state_space==next_state)[0]
         else:
             next_state_index = action_index
             reward = -1 * action_index
             is_terminal = None
+        self.episode_reward += reward
         return reward, next_state_index, is_terminal
 
     def is_terminal_state(self, state_index):
@@ -147,12 +184,25 @@ class Agent():
 
     def get_c(self, state_index, action_index):
         c = 1
-        # TODO : c needs to be adapted to each algorithm (retrace, tree-backup...)
         if self.algorithm_used == 'tb':
-            if np.max(self.q[state_index, :]) == self.q[state_index, action_index]:
-                c = 1 - self.epsilon + self.epsilon / self.action_count
-            else:
+            if self.q[state_index, action_index] < np.max(self.q[state_index, :]):
                 c = self.epsilon / self.action_count
+            else:
+                c = 1 - self.epsilon + self.epsilon / self.action_count
+            c *= self.lbda
+        elif self.algorithm_used == 'is':
+            if self.q[state_index, action_index] < np.max(self.q[state_index, :]):
+                c = self.epsilon
+            else:
+                c = (1 - self.epsilon) * self.action_count + self.epsilon
+        elif self.algorithm_used == 'q_lambda':
+            c = self.lbda
+        elif self.algorithm_used == 'retrace':
+            if self.q[state_index, action_index] < np.max(self.q[state_index, :]):
+                c = self.epsilon
+            else:
+                c = (1 - self.epsilon) * self.action_count + self.epsilon
+            c = self.lbda * np.minimum(c, 1)
         return c
 
     def select_arbitrarly_action(self):
